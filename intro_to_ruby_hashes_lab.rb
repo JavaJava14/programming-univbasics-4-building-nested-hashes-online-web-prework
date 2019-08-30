fc3f4d0028114594db0f5d644fe487ea961b7509
Jose Chavira <joeychavira2016@gmail.com>
2019-08-30 21:46:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,13 +51,13 @@ def monopoly_with_fourth_tier
 					"mortgage_value" => "$100"
 				},
 				shortline_railroad: {
-					"mortgage_value" => "$200"
+					"mortgage_value" => "$800"
 				},
 				pennsylvania_railroad: {
-					"mortgage_value" => "$400"
+					"mortgage_value" => "$200"
 				},
 				b_and_o_railroad: {
-					"mortgage_value" => "$800"
+					"mortgage_value" => "$400"
 				}
 			}
 		}

e5846b215077c51ae2325b664fe4a8b98184c684
Jose Chavira <joeychavira2016@gmail.com>
2019-08-30 21:42:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,19 +34,9 @@ def monopoly_with_third_tier
 		}
 	}
 
-	=begin
-	monopoly = {
-		:railroads => {
-			pieces: 4,
-			:rent_in_dollars => {
-				one_piece_owned: 25,two_pieces_owned: 50,three_pieces_owned: 100, four_pieces_owned: 200
-			},
-				:names => {
-				reading_railroad: {}, shortline_railroad: {}, b_and_o_railroad: {}, pennsylvania_railroad: {}
-			}
-			}
-			 }
-			 = end
+	
+	
+			 
 end
 
 def monopoly_with_fourth_tier
